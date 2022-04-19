661d1702546b456ab5003bb3983d5eef3289ab72
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-19 17:19:00 +0600

    add global directive to capture click outside

--- src/main.js
+++ src/main.js
@@ -7,6 +7,20 @@ Vue.config.productionTip = false
 
 Vue.use(VueCompositionApi)
 
+Vue.directive('click-outside', {
+  bind: function (el, binding, vnode) {
+    el.event = function (event) {
+      if (!(el == event.target || el.contains(event.target))) {
+        vnode.context[binding.expression](event, binding.arg);
+      }
+    };
+    document.body.addEventListener('click', el.event)
+  },
+  unbind: function (el) {
+    document.body.removeEventListener('click', el.event)
+  },
+});
+
 new Vue({
   render: h => h(App)
 }).$mount('#app')
