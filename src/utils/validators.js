91b9e2edbf8bc4a8baf1be1d4434b8ae50b7dc51
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-20 18:29:33 +0600

    format validator

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,2 +1,2 @@
-export const mustBeInPast = (value) => new Date() > new Date(value)
+export const mustBeInPast = (value) => !value || new Date() > new Date(value) // показывать, только если пользователь ввел дату
 export const isStartsWithSeven = (value) => value[0] === '7'
